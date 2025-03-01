a72cf47f2dfac5465951573c2b4d2ba92e60a1df
ashhar <ashharulhaque@gmail.com>
2025-03-02 01:13:37 +0530

    done

--- src/app/_homepage/homepage.jsx
+++ src/app/_homepage/homepage.jsx
@@ -10,21 +10,21 @@ import TestimonialCarousel from "@homepage/testiomial"
         <>
         <div className="min-h-screen bg-[url('/logo.png')] bg-no-repeat bg-center bg-fixed bg-cover">
         <HeroSection/>
-        <h2 className="flex justify-center text-4xl font-extrabold uppercase tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-400 m-[100px] bg-gradient-to-r from-gray-50 via-gray-100 to-indigo-400 text-transparent bg-clip-text text-center font-bold text-3xl p-2 sm:text-5xl lg:text-7xl leading-tight tracking-tighter">
-WHO ARE WE
+        <h2 className="headingTitle">
+WHO ARE WEee
 </h2>
 <AboutUs/>
-        <h2 className="flex justify-center text-4xl font-extrabold uppercase tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-400 m-[100px]">
+        <h2 className="headingTitle">
   OUR SERVICES
 </h2>
 
 <DoubleArticle/>
 
         
-        <h2 className="flex justify-center text-4xl font-extrabold uppercase tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-400 m-[100px]">
+        <h2 className="headingTitle">
         OUR CLIENTS</h2>
         <Clients/>
-        <h2 className="flex justify-center text-4xl font-extrabold uppercase tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-400 m-[100px]">
+        <h2 className="headingTitle">
         Testimonials</h2>
         {/* <Testimonial />
          */}
