199127f174c6968f03c9adade14718ae8617173a
ashhar <ashharulhaque@gmail.com>
2025-03-01 15:29:33 +0530

    double section implemented

--- src/app/_homepage/homepage.jsx
+++ src/app/_homepage/homepage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import HeroSection from "@homepage/heroSection";
 import Carousel from "@/ui/carousel";
+import DoubleArticle from "@homepage/doubleSection";
  function HomePage(){
     return(
         <>
         <HeroSection/>
+        <DoubleArticle/>
         <Carousel />
+      
         </>
     )
 }
