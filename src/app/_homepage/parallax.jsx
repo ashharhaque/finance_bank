cd40159b1a5ce7b05e0ebb110c3fa27fc06c9841
ashhar <ashharulhaque@gmail.com>
2025-03-02 13:14:45 +0530

    done

--- src/app/_homepage/parallax.jsx
+++ src/app/_homepage/parallax.jsx
@@ -9,16 +9,16 @@ function ContentFixedSlide() {
       </h1>
 
       <div className="sticky top-1/3">
-      <div className=" bg-blue-800 w-full aspect-video sticky top-1/3">
+      <div className="bg-white  w-full aspect-video ">
         {/* <ContentWithBgImg className="bg-[url('/websitebackgroundimage4.jpeg')] bg-cover bg-center bg-no-repeat min-h-screen flex flex-col justify-center w-full aspect-video"> */}
-          <h1 className=" flex justify-center items-center font-bold text-xl md:text-2xl">
+          <h1 className="sticky top-1/3 flex justify-center items-center font-bold text-xl md:text-2xl">
             Background image component is created 11
           </h1>
         {/* </ContentWithBgImg> */}
       </div>
       </div>
       <div className="sticky top-1/3">
-      <div className="bg-yellow-500 w-full aspect-video">
+      <div className="bg-white  w-full aspect-video">
         {/* <ContentWithBgImg className="bg-[url('/boardroombackground10.jpeg')] bg-cover bg-center bg-no-repeat min-h-screen flex flex-col justify-center w-full h-full grayscale"> */}
           <h1 className="sticky top-1/3 flex justify-center items-center font-bold text-xl md:text-2xl">
             Background image component is created 2
@@ -28,7 +28,7 @@ function ContentFixedSlide() {
       </div>
    
       <div className="sticky top-1/3">
-      <div className="bg-red-500 w-full aspect-video">
+      <div className="bg-white  w-full aspect-video">
         {/* <ContentWithBgImg className="bg-[url('/images/bg.jpg')] bg-cover bg-center bg-no-repeat min-h-screen flex flex-col justify-center w-full h-full grayscale"> */}
           <h1 className="sticky top-1/3 flex justify-center items-center font-bold text-xl md:text-2xl">
             Background image component is created 3
@@ -37,11 +37,11 @@ function ContentFixedSlide() {
       </div>
       </div>
     {/* last slide to make sticky */}
-    <div className="sticky top-1/3 h-[700px]">
+    <div className="sticky top-1/3">
       <div className="bg-white w-full aspect-video">
         {/* <ContentWithBgImg className="bg-[url('/images/bg.jpg')] bg-cover bg-center bg-no-repeat min-h-screen flex flex-col justify-center w-full h-full grayscale"> */}
           <h1 className="sticky top-1/3 flex justify-center items-center font-bold text-xl md:text-2xl">
-            Background image component is created 3
+            Background image component is created 4
           </h1>
         {/* </ContentWithBgImg> */}
       </div>
