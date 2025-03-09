7a336d72fb3c987195a662d8ccd1ff578f97ea23
ashhar <ashharulhaque@gmail.com>
2025-03-09 13:30:45 +0530

    website done

---
+++ src/app/_homepage/clientsNew.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Image from "next/image";
+import Client1 from "@/public/c1.png";
+import Client2 from "@/public/c2.png";
+import Client3 from "@/public/c4.png";
+import Client4 from "@/public/a3.png";
+import Client5 from "@/public/c5.png";
+import Client6 from "@/public/c7.png";
+import Client7 from "@/public/c8.png";
+import Client8 from "@/public/c10.png";
+import Client9 from "@/public/c14.png";
+import Client10 from "@/public/c15.jpg";
+import Client11 from "@/public/c16.png";
+import Client12 from "@/public/c17.png";
+import Client13 from "@/public/c18.jpg";
+import Client14 from "@/public/c19.jpg";
+import Client15 from "@/public/c20.jpg";
+import Client16 from "@/public/c21.jpg";
+import Client17 from "@/public/c1.png";
+function Clients() {
+  const clientsImageArray = [
+    Client1,
+    Client2,
+    Client3,
+    Client4,
+    Client5,
+    Client6,
+    Client7,
+    Client8,
+    Client9,
+    Client10,
+    Client11,
+    Client12,
+    Client13,
+    Client14,
+    Client15,
+    Client16,
+    Client17,
+  ];
+  return (
+    <>
+      <div className="flex flex-wrap justify-center ">
+        
+        {clientsImageArray
+          .map((client) => (
+            <div className="m-3 p-3">
+              <Image src={client}></Image>
+            </div>
+          ))}
+       
+       
+      </div>
+    </>
+  );
+}
+
+export default Clients;
--- src/app/_homepage/homepage.jsx
+++ src/app/_homepage/homepage.jsx
@@ -2,57 +2,48 @@ import React from "react";
 import HeroSection from "@homepage/heroSection";
 import Testimonial from "@/ui/carousel";
 import DoubleArticle from "@homepage/doubleSection";
-import Clients from "@homepage/clients";
-import AboutUs from "@homepage/parallax"
-import TestimonialCarousel from "@homepage/testiomial"
- function HomePage(){
-    return(
-        <>
-        <div className="min-h-screen bg-image">
-        <HeroSection/>
+import Clients from "@homepage/clientsNew";
+import AboutUs from "@homepage/parallax";
+import TestimonialCarousel from "@homepage/testiomial";
+function HomePage() {
+  return (
+    <>
+      <div className="min-h-screen bg-image">
+        <HeroSection />
         <div>
-        <h2 className="headingTitle sticky top-0 z-20 ">
-WHO ARE WEee
-</h2>
-<div className="sticky top-1/3">
-<AboutUs/>
-
-</div>
+          <h2 className="headingTitle sticky top-0 z-20 ">WHO ARE WEee</h2>
+          <div className="sticky top-1/3">
+            <AboutUs />
+          </div>
         </div>
-<div className="relative">
-{/* <h2 className="headingTitle sticky top-0 z-20 ">
+        <div className="relative">
+          {/* <h2 className="headingTitle sticky top-0 z-20 ">
   OUR SERVICESeesss
 </h2> */}
-<div className="relative sticky top-1/3 z-100">
-<h2 className="headingTitle sticky top-0 z-20 ">
-  OUR SERVICESeesss
-</h2>
-<DoubleArticle/>
-</div>
-
-</div>
-
-
-        <div>
-        <h2 className="headingTitle sticky top-0 ">
-        OUR CLIENTS</h2>
-        <Clients/>
-        </div>
-       <div>
-        <div>
-        <h2 className="headingTitle sticky top-0">
-        Testimonials</h2>
-        {/* <Testimonial />
-         */}
-        
-   <TestimonialCarousel/>
+          <div className="relative sticky top-1/3 z-100">
+            <h2 className="headingTitle sticky top-0 z-20 ">
+              OUR SERVICESeesss
+            </h2>
+            <DoubleArticle />
+          </div>
         </div>
 
-       </div>
-  
-   </div>
-        </>
-    )
+        <div className="bgColor">
+          <h2 className="headingTitle ">OUR CLIENTS</h2>
+          <Clients />
+        </div>
+        <div>
+          <div className="bgColor">
+            <h2 className="headingTitle ">Testimonials</h2>
+            {/* <Testimonial />
+             */}
+
+            <TestimonialCarousel />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
