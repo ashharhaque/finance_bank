61e9c7c4d937261646a9886846d78094b90babc5
ashhar <ashharulhaque@gmail.com>
2025-06-22 10:49:50 +0530

    done headers

--- src/app/_commonComponents/_layout/navbar.jsx
+++ src/app/_commonComponents/_layout/navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
-import Link from 'next/link';
+import Link from "next/link";
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -9,30 +10,27 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
+    <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900 shadow-sm">
       <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+        {/* Logo */}
         <a href="#" className="flex items-center">
-          {/* <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                        Company Logo
-                    </span> */}
-          {/**logo starts */}
           <div className="text-4xl font-extrabold tracking-wide text-gray-800">
             Team
             <span className="bg-gradient-to-r from-teal-400 to-teal-600 bg-clip-text text-transparent">
               Works
             </span>
           </div>
-
-          {/*logo ends */}
         </a>
 
+        {/* Contact Button + Mobile Toggle */}
         <div className="flex items-center lg:order-2">
-          <Link   href="#contact-us"  className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
-          Contact Us
-       
+          <Link
+            href="#contact-us"
+            className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-semibold rounded-full text-sm px-4 py-2 lg:mr-4 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
+          >
+            Contact Us
           </Link>
-        
-         
+
           <button
             onClick={toggleMenu}
             className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -44,7 +42,6 @@ const Navbar = () => {
                 className="w-6 h-6"
                 fill="currentColor"
                 viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   fillRule="evenodd"
@@ -57,7 +54,6 @@ const Navbar = () => {
                 className="w-6 h-6"
                 fill="currentColor"
                 viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   fillRule="evenodd"
@@ -69,30 +65,27 @@ const Navbar = () => {
           </button>
         </div>
 
+        {/* Menu Links */}
         <div
           className={`items-center justify-between w-full lg:flex lg:w-auto lg:order-1 ${
             isOpen ? "block" : "hidden"
           }`}
           id="mobile-menu-2"
         >
-          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-4 lg:mt-0">
+          <ul className="flex flex-col space-y-1 mt-4 font-semibold text-base lg:flex-row lg:space-y-0 lg:gap-x-2 lg:mt-0">
             {[
-             { name: "Home", link: "#home" },
-             { name: "About Us", link: "#about-us" },
-             { name: "Services", link: "#our-services" },
-             { name: "Founders", link: "#ourfounders" },
-              
+              { name: "Home", link: "#home" },
+              { name: "About Us", link: "#about-us" },
+              { name: "Services", link: "#our-services" },
+              { name: "Founders", link: "#ourfounders" },
             ].map((item) => (
-              <li key={item}>
-                <Link href={item.link}  className="block py-1.5 px-4 text-gray-700 border border-transparent
-                hover:bg-orange-100 hover:border-orange-500 rounded-full transition-all duration-300
-                dark:text-gray-400 dark:hover:bg-orange-700 dark:hover:border-orange-500">{item?.name}</Link>
-                {/* <a
-                  href="#"
-                 
+              <li key={item.name}>
+                <Link
+                  href={item.link}
+                  className="block py-2 px-3 text-gray-800 dark:text-white hover:bg-orange-100 dark:hover:bg-orange-600 hover:text-orange-800 dark:hover:text-white transition-all duration-300 rounded-full"
                 >
-                  
-                </a> */}
+                  {item.name}
+                </Link>
               </li>
             ))}
           </ul>
